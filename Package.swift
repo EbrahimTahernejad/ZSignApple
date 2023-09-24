6c2b6bc4926c36a1c23e04041191da1300b9cc50
Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>
2023-09-24 05:44:23 +0330

    initial version

--- Package.swift
+++ Package.swift
@@ -1,23 +1,35 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ZSignApple",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZSignApple",
-            targets: ["ZSignApple"]),
+            targets: ["ZSign"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZSignApple"),
-        .testTarget(
-            name: "ZSignAppleTests",
-            dependencies: ["ZSignApple"]),
-    ]
+            name: "ZSign",
+            dependencies: ["OpenSSL"],
+            publicHeadersPath: "./Includes",
+            cxxSettings: [
+                .headerSearchPath("."),
+            ],
+            linkerSettings: [
+                .linkedFramework("OpenSSL"),
+            ]
+        ),
+        .binaryTarget(
+            name: "OpenSSL",
+            path: "Binaries/OpenSSL.xcframework"
+        ),
+    ],
+    cxxLanguageStandard: .cxx14
 )
---
+++ Sources/ZSign/Includes/ZSign.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  
+//
+//  Created by Ebrahim Tahernejad on 7/2/1402 AP.
+//
+
+#ifndef ZSign_h
+#define ZSign_h
+
+#include "zsign.h"
+
+#endif /* ZSign_h */
